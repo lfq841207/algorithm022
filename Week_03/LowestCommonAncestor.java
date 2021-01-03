f2e42493fc49a43fe43df4eda2c7002222feb8e6
GeffreyLi <lfq841207@163.com>
2021-01-03 23:12:15 +0800

    二叉树的最近公共祖先-修改类名

--- Week_03/LowestCommonAncestor.java
+++ Week_03/LowestCommonAncestor.java
@@ -1,4 +1,4 @@
-class Solution {
+public class LowestCommonAncestor {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
             return root;
